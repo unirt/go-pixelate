aa8a546b654feb62fde090b569f502ae1ce4e1d8
unirt <lunirtc@gmail.com>
2019-12-14 09:33:59 +0900

    v0.1

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # go-tile
+Convert image to tile
+## Usage
+### Steps
+1. Clone this repository
+   ```bash
+   git clone git@github.com:unirt/go-tile.git
+   ```
+2. Convert image to tile
+   ```bash
+   cd go-tile
+   go run main.go -i path/to/image -n 4
+   # converted image path will be `./outputs/16tile_{originalImagePath}.png`
+   ```
+### Args
+|arg  |must/optional  |description  |
+|:-:|:-:|:-:|
+|i  |must  |Path to original image  |
+|n  |optional(default `4`)|The square root of the number of tiles in converted image. <br> ex. Converted image will have 16 tiles if you provide `-n 4`|
+## Example
+```bash
+go run main.go -i ~/Pictures/sample.png
+```
+Original(`sample.png`)  
+![sample.png](.github/sample.jpg)  
+Result(`16tile_sample.png`)  
+![16tile_sample.png](.github/16tiled_sample.png)
--- main.go
+++ main.go
@@ -1,49 +1,131 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"image"
+	"image/color"
 	_ "image/jpeg"
+	"image/png"
 	_ "image/png"
 	"os"
+	"path"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Starting program...")
-	file, err := os.Open("images/image2.jpg")
-	defer file.Close()
+	inputImagePath, side, err := getFlags()
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
 
-	img, t, err := image.Decode(file)
+	originalFile, err := os.Open(inputImagePath)
+	defer originalFile.Close()
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
-	fmt.Println("Type of image:", t)
 
-	rct := img.Bounds()
-	fmt.Println("Width:", rct.Dx())
-	fmt.Println("Height:", rct.Dy())
-	r, g, b, a := img.At(1051, 1059).RGBA()
-	fmt.Println("pixel:", r/257, g/257, b/257, a/257)
+	originalImage, _, err := image.Decode(originalFile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	var rr, gg, bb, aa uint32
-	for i := 0; i < rct.Dx(); i++ {
-		for j := 0; j < rct.Dy(); j++ {
-			r, g, b, a = img.At(i, j).RGBA()
-			rr += r / 257
-			gg += g / 257
-			bb += b / 257
-			aa += a / 257
+	originalRect := originalImage.Bounds()
+	width, height, rect := getNewRect(originalRect, side)
+
+	colors := getColors(originalImage, width, height, side)
+
+	newImage := createNewImage(colors, side, width, height, rect)
+
+	name := getNewImageName(originalFile.Name(), side)
+	f, err := os.Create("./outputs/" + name)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	png.Encode(f, newImage)
+
+	fmt.Println("Tiled!")
+	fmt.Println("Here is tiled image: " + f.Name())
+}
+
+func getFlags() (inputPath string, sideNum int, err error) {
+	inputImagePath := flag.String("i", "", "Path to original image")
+	side := flag.Int("n", 4, "Number for converting nxn tile")
+
+	flag.Parse()
+	if *inputImagePath == "" {
+		return "", *side, errors.New("[Error] Path to original image was not found")
+	}
+
+	return *inputImagePath, *side, nil
+}
+
+func getNewRect(originalRect image.Rectangle, side int) (width int, height int, rect image.Rectangle) {
+	width = (originalRect.Dx() / side) * side
+	height = (originalRect.Dy() / side) * side
+
+	upLeft := image.Point{0, 0}
+	lowRight := image.Point{width, height}
+	rect = image.Rectangle{upLeft, lowRight}
+	return
+}
+
+func getColors(originalImage image.Image, width int, height int, side int) (colors []color.RGBA) {
+	const NumToDiv = 257
+
+	for sec := 0; sec < side*side; sec++ {
+		secWidth := (width / side) * (sec/side + 1)
+		secHeight := (height / side) * (sec%side + 1)
+		var sumR, sumG, sumB, sumA uint32
+		for x := 0; x < secWidth; x++ {
+			for y := 0; y < secHeight; y++ {
+				r, g, b, a := originalImage.At(x, y).RGBA()
+				sumR += r / NumToDiv
+				sumG += g / NumToDiv
+				sumB += b / NumToDiv
+				sumA += a / NumToDiv
+			}
+		}
+		numOfElem := uint32(secWidth * secHeight)
+		aveR := sumR / numOfElem
+		aveG := sumG / numOfElem
+		aveB := sumB / numOfElem
+		aveA := sumA / numOfElem
+
+		colors = append(colors, color.RGBA{uint8(aveR), uint8(aveG), uint8(aveB), uint8(aveA)})
+	}
+	return
+}
+
+func createNewImage(colors []color.RGBA, side int, width int, height int, rect image.Rectangle) (newImage *image.RGBA) {
+	newImage = image.NewRGBA(rect)
+
+	for sec := 0; sec < side*side; sec++ {
+		secStartWidth := (width / side) * (sec / side)
+		secEndWidth := (width / side) * (sec/side + 1)
+		secStartHeight := (height / side) * (sec % side)
+		secEndHeight := (height / side) * (sec%side + 1)
+
+		col := colors[sec]
+		for x := secStartWidth; x < secEndWidth; x++ {
+			for y := secStartHeight; y < secEndHeight; y++ {
+				newImage.Set(x, y, col)
+			}
 		}
 	}
-	m := uint32(rct.Dx() * rct.Dy())
-	rr /= m
-	gg /= m
-	bb /= m
-	aa /= m
-	fmt.Println("Ave pixel", rr, gg, bb, aa)
+	return
+}
+
+func getNewImageName(originalName string, side int) string {
+	nameS := strings.Split(originalName, "/")
+	name := nameS[len(nameS)-1]
+	ext := path.Ext(name)
+
+	return strconv.Itoa(side*side) + "tile_" + name[0:len(name)-len(ext)] + ".png"
 }
